2384ae8fdba8b84a22dab68e694da753ce708548
Mateus Silva <127849926+Josmateussilva0110@users.noreply.github.com>
2023-04-26 17:49:53 -0300

    Add files via upload

---
+++ C/1059.py
@@ -0,0 +1,4 @@
+c = 1
+for t in range(c, 101):
+    if t % 2 == 0:
+        print(f'{t}')
---
+++ C/1065.py
@@ -0,0 +1,7 @@
+c = 0
+cont = 0
+for t in range(c, 5):
+    n = int(input())
+    if n % 2 == 0:
+        cont += 1
+print(f'{cont} valores pares')
